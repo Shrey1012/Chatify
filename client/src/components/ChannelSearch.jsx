da25241bdc335fe2933e1bb1fdc8e048f8866d45
Shrey1012 <shrey9157@gmail.com>
2022-07-08 23:01:04 +0530

    Added Search Component

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,27 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { getChannel, useChatContext } from "stream-chat-react";
+import { useChatContext } from "stream-chat-react";
+
+import {ResultsDropdown} from './'
 
 import { SearchIcon } from "../assets";
 
-const ChannelSearch = () => {
-    const [query, setQuery] = useState("");
-    const [loading,setLoading] = useState("");
+const ChannelSearch = ({setToggleContainer}) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState("");
+  const [teamChannels, setTeamChannels] = useState([]);
+  const [directChannels, setDirectChannels] = useState([]);
 
-    const getChannels = async (text) => {
-        try {
-            
-        } catch (error) {
-            setQuery('');
-        }
+  useEffect(() => {
+    if(!query){
+      setTeamChannels([]);
+      setDirectChannels([]);
     }
+  },[query]);
 
-    const onSearch = (event) => {
-        event.preventDefault();
+  const getChannels = async (text) => {
+    try {
+      const channelResponse = client.queryChannels({
+        type: "team",
+        name: { $autocomplete: text },
+        members: { $in: [client.userID] },
+      });
+      const userResponse = client.queryUsers({
+        id: {$ne: client.userID},
+        name: {$autocomplete: text}
+      })
 
-        setLoading(true);
-        setQuery(event.target.value);
-        getChannels(event.target.value)
+      const [channels,{users}] = await Promise.all([channelResponse,userResponse]);
+
+      if(channels.length) setTeamChannels(channels);
+      if(users.length) setDirectChannels(users);
+
+    } catch (error) {
+      setQuery("");
     }
+  };
+
+  const onSearch = (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+    setQuery(event.target.value);
+    getChannels(event.target.value);
+  };
+
+  const setChannel = (channel) =>{
+    setQuery('');
+    setActiveChannel(channel);
+  }
 
   return (
     <div className="channel-search__container">
@@ -37,6 +68,16 @@ const ChannelSearch = () => {
           onChange={onSearch}
         />
       </div>
+      { query && (
+        <ResultsDropdown
+        teamChannels={teamChannels}
+        directChannels={directChannels}
+        loading={loading}
+        setChannel={setChannel}
+        setQuery={setQuery}
+        setToggleContainer={setToggleContainer}
+        />
+      )}
     </div>
   );
 };
