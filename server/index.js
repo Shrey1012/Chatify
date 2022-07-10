9e2a6c030dcfc5e2e9ad56e413ab01f5f5d173d7
Shrey1012 <shrey9157@gmail.com>
2022-07-10 15:13:59 +0530

    Fixed notification feature bug

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.get('/',(req,res)=>{
     res.send('hello world');
 });
 
-app.post('./',(req,res)=>{
+app.post('/',(req,res)=>{
     const {message, user: sender, type, members} = req.body;
 
     if(type === 'message.new'){
